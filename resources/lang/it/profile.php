f351ecbbb6c217c72987a345a6ca2385a6378f51
marco76tv <marco.sottana@gmail.com>
2025-01-09 09:02:27 +0100

    up

---
+++ resources/lang/it/profile.php
@@ -0,0 +1,72 @@
+<?php return array (
+  'actions' => 
+  array (
+    'create' => 
+    array (
+      'label' => 'create',
+    ),
+  ),
+  'fields' => 
+  array (
+    'id' => 
+    array (
+      'label' => 'id',
+    ),
+    'type' => 
+    array (
+      'label' => 'tipo',
+    ),
+    'first_name' => 
+    array (
+      'label' => 'Nome',
+    ),
+    'last_name' => 
+    array (
+      'label' => 'Cognome',
+    ),
+    'full_name' => 
+    array (
+      'label' => 'full_name',
+    ),
+    'email' => 
+    array (
+      'label' => 'Email',
+    ),
+    'created_at' => 
+    array (
+      'label' => 'created_at',
+    ),
+    'updated_at' => 
+    array (
+      'label' => 'updated_at',
+    ),
+    'user_id' => 
+    array (
+      'label' => 'user_id',
+    ),
+    'updated_by' => 
+    array (
+      'label' => 'updated_by',
+    ),
+    'created_by' => 
+    array (
+      'label' => 'created_by',
+    ),
+    'deleted_at' => 
+    array (
+      'label' => 'deleted_at',
+    ),
+    'deleted_by' => 
+    array (
+      'label' => 'deleted_by',
+    ),
+    'is_active' => 
+    array (
+      'label' => 'Stato account',
+    ),
+    'edit' => 
+    array (
+      'label' => 'edit',
+    ),
+  ),
+);
