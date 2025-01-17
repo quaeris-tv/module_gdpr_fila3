4e068b59a90ec3ec14b31c1e7b37f0d440c03ef2
marco76tv <marco.sottana@gmail.com>
2025-01-17 15:59:54 +0100

    up

--- app/Filament/Resources/ProfileResource.php
+++ app/Filament/Resources/ProfileResource.php
@@ -16,41 +16,40 @@ class ProfileResource extends XotBaseResource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    public static function form(Form $form): Form
+    public static function getFormSchema(): array
     {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('type')
-                    ->maxLength(255)
-                    ->default(null),
-                Forms\Components\TextInput::make('first_name')
-                    ->maxLength(191)
-                    ->default(null),
-                Forms\Components\TextInput::make('last_name')
-                    ->maxLength(191)
-                    ->default(null),
-                Forms\Components\TextInput::make('full_name')
-                    ->maxLength(191)
-                    ->default(null),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->maxLength(191)
-                    ->default(null),
-                Forms\Components\TextInput::make('user_id')
-                    ->maxLength(36)
-                    ->default(null),
-                Forms\Components\TextInput::make('updated_by')
-                    ->maxLength(36)
-                    ->default(null),
-                Forms\Components\TextInput::make('created_by')
-                    ->maxLength(36)
-                    ->default(null),
-                Forms\Components\TextInput::make('deleted_by')
-                    ->maxLength(36)
-                    ->default(null),
-                Forms\Components\Toggle::make('is_active')
-                    ->required(),
-            ]);
+        return [
+            Forms\Components\TextInput::make('type')
+                ->maxLength(255)
+                ->default(null),
+            Forms\Components\TextInput::make('first_name')
+                ->maxLength(191)
+                ->default(null),
+            Forms\Components\TextInput::make('last_name')
+                ->maxLength(191)
+                ->default(null),
+            Forms\Components\TextInput::make('full_name')
+                ->maxLength(191)
+                ->default(null),
+            Forms\Components\TextInput::make('email')
+                ->email()
+                ->maxLength(191)
+                ->default(null),
+            Forms\Components\TextInput::make('user_id')
+                ->maxLength(36)
+                ->default(null),
+            Forms\Components\TextInput::make('updated_by')
+                ->maxLength(36)
+                ->default(null),
+            Forms\Components\TextInput::make('created_by')
+                ->maxLength(36)
+                ->default(null),
+            Forms\Components\TextInput::make('deleted_by')
+                ->maxLength(36)
+                ->default(null),
+            Forms\Components\Toggle::make('is_active')
+                ->required(),
+        ];
     }
 
     public static function getRelations(): array
