28aab295e5bd0611fbf878ecd1b15045788e4994
marco76tv <1944941+marco76tv@users.noreply.github.com>
2025-02-18 20:46:08 +0000

    Lint

--- app/Filament/Clusters/Profile/Resources/ProfileResource/Pages/EditProfile.php
+++ app/Filament/Clusters/Profile/Resources/ProfileResource/Pages/EditProfile.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Clusters\Profile\Resources\ProfileResource\Pages;
 
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Modules\Gdpr\Filament\Clusters\Profile\Resources\ProfileResource;
 
 class EditProfile extends EditRecord
 {
     protected static string $resource = ProfileResource::class;
-
-   
 }
--- app/Filament/Resources/TreatmentResource/Pages/EditTreatment.php
+++ app/Filament/Resources/TreatmentResource/Pages/EditTreatment.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Resources\TreatmentResource\Pages;
 
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Modules\Gdpr\Filament\Resources\TreatmentResource;
 
 class EditTreatment extends EditRecord
 {
     protected static string $resource = TreatmentResource::class;
-
-   
 }
