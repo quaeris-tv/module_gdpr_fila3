5c0da95ef9d8baab38c203ec87ffc9d8e11c754f
marco76tv <1944941+marco76tv@users.noreply.github.com>
2025-02-18 20:46:08 +0000

    Lint

--- app/Filament/Resources/ConsentResource/Pages/EditConsent.php
+++ app/Filament/Resources/ConsentResource/Pages/EditConsent.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Resources\ConsentResource\Pages;
 
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Modules\Gdpr\Filament\Resources\ConsentResource;
 
 class EditConsent extends EditRecord
 {
     protected static string $resource = ConsentResource::class;
-
-   
 }
--- app/Filament/Resources/ProfileResource/Pages/EditProfile.php
+++ app/Filament/Resources/ProfileResource/Pages/EditProfile.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Filament\Resources\ProfileResource\Pages;
 
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Modules\Gdpr\Filament\Resources\ProfileResource;
 
 class EditProfile extends EditRecord
 {
     protected static string $resource = ProfileResource::class;
-
-   
 }
