1d391f8c59f2a8a92c5bfbda24e6263217d01508
marco xot <marco.sottana@gmail.com>
2025-02-26 11:55:20 +0100

    .

--- Models/Consent.php
+++ Models/Consent.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+<<<<<<< HEAD
+=======
+use Illuminate\Support\Carbon;
+>>>>>>> 4a1eeab7 (Lint)
 
 /**
  * Modules\Gdpr\Models\Consent.
@@ -15,6 +19,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property string                              $id
  * @property string                              $treatment_id
  * @property string                              $subject_id
+<<<<<<< Updated upstream
  * @property Carbon|null                         $created_at
  * @property Carbon|null                         $updated_at
  * @property \Modules\Gdpr\Models\Treatment|null $treatment
@@ -28,6 +33,37 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @method static \Illuminate\Database\Eloquent\Builder|Consent                                 whereSubjectId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Consent                                 whereTreatmentId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Consent                                 whereUpdatedAt($value)
+=======
+<<<<<<< HEAD
+ * @property \Illuminate\Support\Carbon|null     $created_at
+ * @property \Illuminate\Support\Carbon|null     $updated_at
+ * @property \Modules\Gdpr\Models\Treatment|null $treatment
+ *
+ * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereSubjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereTreatmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Consent   whereUpdatedAt($value)
+=======
+ * @property Carbon|null                         $created_at
+ * @property Carbon|null                         $updated_at
+ * @property \Modules\Gdpr\Models\Treatment|null $treatment
+ *
+ * @method static \Modules\Gdpr\Database\Factories\ConsentFactory factory($count = null, $state = [])
+ * @method static Builder|Consent                                 newModelQuery()
+ * @method static Builder|Consent                                 newQuery()
+ * @method static Builder|Consent                                 query()
+ * @method static Builder|Consent                                 whereCreatedAt($value)
+ * @method static Builder|Consent                                 whereId($value)
+ * @method static Builder|Consent                                 whereSubjectId($value)
+ * @method static Builder|Consent                                 whereTreatmentId($value)
+ * @method static Builder|Consent                                 whereUpdatedAt($value)
+>>>>>>> 4a1eeab7 (Lint)
+>>>>>>> Stashed changes
  *
  * @mixin \Eloquent
  */
