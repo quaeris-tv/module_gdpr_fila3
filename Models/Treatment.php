0b74afba08a9885382db4549ca3b95d114383a41
marco76tv <marco.sottana@gmail.com>
2025-03-12 12:22:07 +0100

    .

--- Models/Treatment.php
+++ Models/Treatment.php
@@ -4,41 +4,24 @@ declare(strict_types=1);
 
 namespace Modules\Gdpr\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Support\Carbon;
 
 /**
  * Modules\Gdpr\Models\Treatment.
  *
- * @property string                          $id
- * @property int                             $active
- * @property int                             $required
- * @property string                          $name
- * @property string                          $description
- * @property string|null                     $documentVersion
- * @property string|null                     $documentUrl
- * @property int                             $weight
- * @property Carbon|null                     $created_at
- * @property Carbon|null                     $updated_at
+ * @property string      $id
+ * @property int         $active
+ * @property int         $required
+ * @property string      $name
+ * @property string      $description
+ * @property string|null $documentVersion
+ * @property string|null $documentUrl
+ * @property int         $weight
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
  *
  * @method static \Modules\Gdpr\Database\Factories\TreatmentFactory factory($count = null, $state = [])
-<<<<<<< HEAD
- * @method static Builder|Treatment                                 newModelQuery()
- * @method static Builder|Treatment                                 newQuery()
- * @method static Builder|Treatment                                 query()
- * @method static Builder|Treatment                                 whereActive($value)
- * @method static Builder|Treatment                                 whereCreatedAt($value)
- * @method static Builder|Treatment                                 whereDescription($value)
- * @method static Builder|Treatment                                 whereDocumentUrl($value)
- * @method static Builder|Treatment                                 whereDocumentVersion($value)
- * @method static Builder|Treatment                                 whereId($value)
- * @method static Builder|Treatment                                 whereName($value)
- * @method static Builder|Treatment                                 whereRequired($value)
- * @method static Builder|Treatment                                 whereUpdatedAt($value)
- * @method static Builder|Treatment                                 whereWeight($value)
-=======
->>>>>>> origin/dev
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Treatment   query()
@@ -66,7 +49,7 @@ class Treatment extends BaseModel
         'description',
         'documentVersion',
         'documentUrl',
-        'weight'
+        'weight',
     ];
 
     // protected $table = 'treatment';
